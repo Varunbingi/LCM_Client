f629f1f6edf6473692c19026d22d2b4de48856e5
Varunbingi <bingivarun27@gmail.com>
2024-09-02 01:46:53 +0530

    final commit

--- src/config/axiosInstance.jsx
+++ src/config/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 const BASE_URL="https://lms-server-m3gr.onrender.com/api/v1";
-const axiosInstance=axios.create();
-
-axiosInstance.defaults.baseURL=BASE_URL;
-axiosInstance.defaults.withCredentials=true;
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true, 
+});
 
 export default axiosInstance;
